776eddd27a579ff94a6dfb1f0ed83f076ae1a6eb
abrehman-odoo <abrehman_odoo@thinksolutionz.org>
2022-07-18 13:43:55 +0500

    first project

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  title = "blogger"
+  // ** Interpolation **
+
+  // title = 'first';
+  // data = 'banking';
+  // creator(){
+  //   return "Our creator is great"
+  // };
+
+  data=200 ;
+
+}
